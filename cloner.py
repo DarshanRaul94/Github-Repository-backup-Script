20686c4f12bf0d85fbabfc9917150603e2f1f1ed
Darshan Raul <dar.raul@gmail.com>
2019-03-26 23:32:53 +0530

    Create cloner.py

---
+++ cloner.py
@@ -0,0 +1,9 @@
+import requests
+import os
+
+response = requests.get('https://api.github.com/users/<repo_name>/repos').json()
+
+for repo in response:
+    url=str(repo['html_url'])
+    cmd='git clone '+url
+    os.system(cmd)
