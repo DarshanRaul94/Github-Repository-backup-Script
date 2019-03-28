edf21d9f6b0f35760f5bbc9bc9eacbfa4f0d4b86
Darshan Raul <dar.raul@gmail.com>
2019-03-28 23:50:19 +0530

    Update cloner.py

--- cloner.py
+++ cloner.py
@@ -2,11 +2,11 @@
 import requests
 import os
 
-response = requests.get('https://api.github.com/users/<repo_name>/repos').json()
+response = requests.get('https://api.github.com/users/<repo_name>/repos').json() #####getting the whole response containing every detail of all repos
 
 for repo in response:
-    url=str(repo['html_url'])
-    cmd='git clone '+url
-    os.system(cmd)
+    url=str(repo['html_url'])######using this to parse the html url of therepo
+    cmd='git clone '+url ####adding it to the git clone cmd
+    os.system(cmd)########## executing the command
 grepcmd='grep -r <pattern>'
 os.system(grepcmd)
